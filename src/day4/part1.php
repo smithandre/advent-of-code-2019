2abf53de724058a93768c4aaede8971b9302c80a
Pierre du Plessis <pierre@pcservice.co.za>
2017-12-04 09:25:23 +0200

    Complete Day 4

--- src/day4/part1.php
+++ src/day4/part1.php
@@ -10,3 +10,9 @@ declare(strict_types=1);
  */
 
 $input = getInput();
+
+return count(array_filter(explode("\n", $input), function ($passphrase) {
+    $words = explode(' ', $passphrase);
+
+    return count(array_unique($words)) === count($words);
+}));
