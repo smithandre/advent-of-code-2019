5aaba7bc1f14993c128f8d9d26c535d09bc0fa76
Andre Smith <andresm@miway.co.za>
2017-12-12 14:32:07 +0200

    [Day 1] Adding day 1 solution

--- src/day1/part1.php
+++ src/day1/part1.php
@@ -1,25 +1,21 @@
 <?php
-
-declare(strict_types=1);
-
 /*
  * This file is part of the adventofcode project.
  *
- * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @author     Andre Smith <smith.cyber@gmail.com>
  * @copyright  Copyright (c) 2017
  */
 
+declare(strict_types=1);
+
 $input = str_split((string) getInput());
 
+$steps = 1;
 $sum = 0;
-
-foreach ($input as $key => $value) {
-    if (isset($input[$key + 1])) {
-        if ($value === $input[$key + 1]) {
-            $sum += $value;
-        }
-    } else if ($value === $input[0]) {
-        $sum += $value;
+foreach ($input as $index => $value) {
+    $compareValue = $input[($index + $steps) % count($input)];
+    if ($value === $compareValue) {
+        $sum += $compareValue;
     }
 }
 
--- src/day1/part2.php
+++ src/day1/part2.php
@@ -1,29 +1,21 @@
 <?php
-
-declare(strict_types=1);
-
 /*
  * This file is part of the adventofcode project.
  *
- * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @author     Andre Smith <smith.cyber@gmail.com>
  * @copyright  Copyright (c) 2017
  */
 
+declare(strict_types=1);
+
 $input = str_split((string) getInput());
 
+$steps = count($input) / 2;
 $sum = 0;
-$check = count($input) / 2;
-
-foreach ($input as $key => $value) {
-    if (isset($input[$key + $check])) {
-        if ($value === $input[$key + $check]) {
-            $sum += ($value);
-        }
-    } else {
-        $index = ($key + $check) - count($input);
-        if ($value === $input[$index]) {
-            $sum += $value;
-        }
+foreach ($input as $index => $value) {
+    $compareValue = $input[($index + $steps) % count($input)];
+    if ($value === $compareValue) {
+        $sum += $compareValue;
     }
 }
 
