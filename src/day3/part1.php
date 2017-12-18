81f34d625ff967757e09de2135dfa632b2657a62
Andre Smith <andresm@miway.co.za>
2017-12-18 09:41:54 +0200

    Day 3 Part 1 completed

--- src/day3/part1.php
+++ src/day3/part1.php
@@ -8,4 +8,60 @@
 
 declare(strict_types=1);
 
-$input = getInput();
+$input = (int) getInput();
+
+$minX = 0;
+$minY = 0;
+$maxX = 0;
+$maxY = 0;
+
+$matrix = [];
+$xPos = 0;
+$yPos = 0;
+
+$direction = 'r';
+for ($i = 1; $i <= $input; $i++) {
+    $matrix[$xPos][$yPos] = $i;
+
+    if ($input === $i) {
+        break;
+    }
+
+    if ($direction === 'r') {
+        $xPos += 1;
+        if ($xPos === $maxX + 1) {
+            $direction = 'u';
+            $maxX += 1;
+            continue;
+        }
+    }
+
+    if ($direction === 'u') {
+        $yPos += 1;
+        if ($yPos === $maxY + 1) {
+            $direction = 'l';
+            $maxY += 1;
+            continue;
+        }
+    }
+
+    if ($direction === 'l') {
+        $xPos -= 1;
+        if ($xPos === $minX - 1) {
+            $direction = 'd';
+            $minX -= 1;
+            continue;
+        }
+    }
+
+    if ($direction === 'd') {
+        $yPos -= 1;
+        if ($yPos === $minY - 1) {
+            $direction = 'r';
+            $minY -= 1;
+            continue;
+        }
+    }
+}
+
+return abs(0 - $xPos) + abs(0 - $yPos);
