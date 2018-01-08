007b6a3c3c76800146b79d3a9eb93c7c50bbd1fe
Andre Smith <andresm@miway.co.za>
2018-01-08 11:22:07 +0200

    Day 3 Part 2

--- src/day3/part2.php
+++ src/day3/part2.php
@@ -8,4 +8,88 @@
 
 declare(strict_types=1);
 
-$input = getInput();
+$input = (int) getInput();
+
+$minX = 0;
+$minY = 0;
+$maxX = 0;
+$maxY = 0;
+
+$matrix[0][0] = 1;
+$xPos = 1;
+$yPos = 0;
+$sum = 0;
+
+$direction = 'r';
+for ($i = 1; $i <= $input; $i++) {
+
+    if ($direction === 'r') {
+        $sum = ($matrix[$xPos - 1][$yPos] ?? 0)
+            + ($matrix[$xPos - 1][$yPos + 1] ?? 0)
+            + ($matrix[$xPos][$yPos - 1] ?? 0)
+            + ($matrix[$xPos + 1][$yPos + 1] ?? 0)
+            + ($matrix[$xPos][$yPos + 1] ?? 0);
+        $matrix[$xPos][$yPos] = $sum;
+
+        if ($xPos === $maxX + 1) {
+            $direction = 'u';
+            $maxX += 1;
+            continue;
+        }
+        $xPos += 1;
+    }
+
+    if ($direction === 'u') {
+        $sum = ($matrix[$xPos][$yPos - 1] ?? 0)
+            + ($matrix[$xPos - 1][$yPos - 1] ?? 0)
+            + ($matrix[$xPos - 1][$yPos] ?? 0)
+            + ($matrix[$xPos - 1][$yPos + 1] ?? 0);
+        $matrix[$xPos][$yPos] = $sum;
+
+        if ($yPos === $maxY + 1) {
+            $direction = 'l';
+            $maxY += 1;
+            continue;
+        }
+        $yPos += 1;
+    }
+
+    if ($direction === 'l') {
+        $sum = ($matrix[$xPos + 1][$yPos] ?? 0)
+            + ($matrix[$xPos + 1][$yPos - 1] ?? 0)
+            + ($matrix[$xPos][$yPos - 1] ?? 0)
+            + ($matrix[$xPos - 1][$yPos - 1] ?? 0);
+
+        $matrix[$xPos][$yPos] = $sum;
+        if ($xPos === $minX - 1) {
+            $direction = 'd';
+            $minX -= 1;
+            continue;
+        }
+        $xPos -= 1;
+    }
+
+    if ($direction === 'd') {
+        $sum = ($matrix[$xPos][$yPos + 1] ?? 0)
+            + ($matrix[$xPos + 1][$yPos + 1] ?? 0)
+            + ($matrix[$xPos + 1][$yPos] ?? 0)
+            + ($matrix[$xPos + 1][$yPos - 1] ?? 0);
+
+        $matrix[$xPos][$yPos] = $sum;
+        if ($yPos === $minY - 1) {
+            $direction = 'r';
+            $minY -= 1;
+            continue;
+        }
+        $yPos -= 1;
+    }
+
+    if ($sum > $input) {
+        break;
+    }
+
+}
+
+var_dump($matrix);
+
+return $sum;
